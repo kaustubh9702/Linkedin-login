5da2e3f345c29e0b3c454adb9dbe104cf40dc0e6
Kaustubh lawale <kaustubhlawale@gmail.com>
2022-03-11 00:00:14 +0530

    Up

--- app.js
+++ app.js
@@ -32,9 +32,8 @@ function getAccessToken(){
     console.log(code);
     secret="mIuZNcY4NZ5sY4OZ";
     goTo="https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code="+ code +"&redirect_uri=https%3A%2F%2Fkaustubh9702.github.io%2FLinkedin-login%2Fredirect.html&client_id=776gr7yjy7wd82&client_secret=" + secret;
-    console.log(goTo);
     const token=new XMLHttpRequest();
-    token.open("POST", goTo);
+    token.open("GET", goTo);
     token.setRequestHeader("Accept", "Access-Control-Allow-Origin");
     token.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
     console.log(token.responseText);
