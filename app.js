24bdbf93e2ebfb955df10caeaee5b42e63e89ba2
Kaustubh lawale <kaustubhlawale@gmail.com>
2022-03-11 12:55:36 +0530

    Update3

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ function getAccessToken(){
     goTo="https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code="+ code +"&redirect_uri=https%3A%2F%2Fkaustubh9702.github.io%2FLinkedin-login%2Fredirect.html&client_id=776gr7yjy7wd82&client_secret=" + secret;
     const token=new XMLHttpRequest();
     token.open("POST", goTo);
-    
     token.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-    console.log(token.responseText);
+    AccessObj=JSON.parse(token.responseText);
+    console.log(AccessObj);
     token.onreadystatechange = function () {
         if (token.readyState === 4){
             console.log(token.responseText);
