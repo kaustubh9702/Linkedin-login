ac595c208e92891b8e2308518bbb1e290b5286aa
Kaustubh lawale <kaustubhlawale@gmail.com>
2022-03-11 13:13:35 +0530

    up

--- app.js
+++ app.js
@@ -36,6 +36,11 @@ function getAccessToken(){
     token.open("POST", goTo);
     token.setRequestHeader("Accept", "Access-Control-Allow-Origin");
     token.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    if(token.status===200){
+        AccessObj=JSON.parse(token.responseText);
+        AccessToken=AccessObj["access_token"];
+        console.log(AccessToken);
+    }
     token.onreadystatechange = function () {
         if (token.readyState === 4){
             console.log(token.responseText);
